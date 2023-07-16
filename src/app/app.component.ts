3f83d6a7cd8e4513aa12840913778fd09ca9dbb9
Cartoffel <jack.schaible@hotmail.com>
2023-07-15 20:08:53 -0600

    Site design iteration 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Jack Schaible's Profile";
   yearsOfExperience = new Date().getFullYear() - 2013;
+  currentYear = new Date().getFullYear();
 }
