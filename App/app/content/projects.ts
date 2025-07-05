08ee659a87d2dd65c465b4a1e54815632bab5bc4
Cartoffel <jack.schaible@hotmail.com>
2025-07-04 20:51:02 -0600

    Fix project urls

--- App/app/content/projects.ts
+++ App/app/content/projects.ts
@@ -4,13 +4,13 @@ const projects = [
     description:
       "A modern, responsive portfolio site built with Next.js and Tailwind CSS.",
     githubLink: "https://github.com/JackSchaible/Profile",
-    webLink: "https://jackschaible.dev", // Replace with actual web URL
+    webLink: "https://www.jackschaible.ca",
   },
   {
     title: "Glyph Editor",
     description: "A small web app I made for designing spell glyphs for D&D.",
     githubLink: "https://github.com/JackSchaible/glyph-editor",
-    webLink: "https://glyph-editor.vercel.app", // Replace with actual web URL
+    webLink: "https://glyphs.jackschaible.ca",
   },
 ];
 export default projects;
