6de34365f282c02f55e925e3c986d86c82cef7dd
Cartoffel <jack.schaible@hotmail.com>
2025-07-20 12:39:20 -0600

    Add GA

--- App/app/layout.tsx
+++ App/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./providers/ThemeProvider";
 import { AuthProvider } from "./providers/AuthProvider";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           </AuthProvider>
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-KN0JK39G8S" />
     </html>
   );
 }
