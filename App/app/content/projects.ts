6893dede80aac6991de4c40d46d8cadd90de4d2d
Cartoffel <jack.schaible@hotmail.com>
2025-08-27 10:51:21 -0600

    Remove Riva test app

--- App/app/content/projects.ts
+++ App/app/content/projects.ts
@@ -16,14 +16,6 @@ const projects = [
     highlight: true,
   },
   {
-    title: "Contact Manager",
-    description:
-      "A full-stack contact management application built with Vue and .NET Core. Deployed on AWS with a comprehensive CI/CD pipeline utilizing ECS/Fargate, Amplify, and a full suite of cloud services.",
-    githubLink: "https://github.com/JackSchaible/riva-test",
-    webLink: "https://riva.jackschaible.ca",
-    highlight: true,
-  },
-  {
     title: "Saturn Recipe Editor",
     description:
       'This is a React + Electron app for editing and managing game-related data for my "Saturn" game project, featuring automatic file system synchronization and a supply chain SVG visualization tool.',
